3f4378936e01727c09303a21a1727dae1d1818c5
theryokato <ryokato@tamu.edu>
2026-06-11 23:57:40 -0500

    fix: made oneko faster

--- src/components/oneko.js
+++ src/components/oneko.js
@@ -21,7 +21,7 @@
   let idleAnimation = null;
   let idleAnimationFrame = 0;
 
-  const nekoSpeed = 10;
+  const nekoSpeed = 20;
   const spriteSets = {
     idle: [[-3, -3]],
     alert: [[-7, -3]],
