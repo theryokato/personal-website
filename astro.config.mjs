bda39cc320a539eaf7ba3e3b2c34e91d370107f5
theryokato <ryokato@tamu.edu>
2026-06-11 23:30:08 -0500

    fix: added wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [mdx()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-light',
       wrap: true,
     },
   },
+
+  adapter: cloudflare(),
 });
