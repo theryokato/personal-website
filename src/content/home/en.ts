bfcefb3ea9648debd747d7558c6897a4206a6d79
theryokato <ryokato@tamu.edu>
2026-07-26 00:21:34 -0500

    feat: separating content from code for home page, faster redirect for 404

---
+++ src/content/home/en.ts
@@ -0,0 +1,34 @@
+export const homeContent = {
+  name: "Ryo Kato",
+  bio: `I'm a third-year mechanical engineering student at <a
+         href="https://www.tamu.edu"
+         class="text-text underline underline-offset-[3px] decoration-1 hover:text-red transition-colors"
+         >Texas A&M University</a
+       >. Currently the Internal VP of <a
+         href="https://turtlerobotics.org"
+         class="text-text underline underline-offset-[3px] decoration-1 hover:text-yellow transition-colors"
+         >TURTLE Robotics</a
+       >, the largest student-led robotics org in Texas. Also helping out
+       research at <a
+         href="https://herc.engr.tamu.edu/"
+         class="text-text underline underline-offset-[3px] decoration-1 hover:text-peach transition-colors"
+         >Human-Empowering Robotics and Control (HERC) Lab</a
+       >.`,
+  sections: [
+    {
+      title: "projects",
+      description: "Things I have worked on.",
+      href: "/en/projects",
+    },
+    {
+      title: "interests",
+      description: "Stuff I like.",
+      href: "/en/interests",
+    },
+    {
+      title: "about me",
+      description: "Who am I?",
+      href: "/en/aboutme",
+    },
+  ],
+};
---
+++ src/content/home/ja.ts
@@ -0,0 +1,33 @@
+export const homeContent = {
+  name: "加藤 遼",
+  bio: `<a
+         href="https://www.tamu.edu"
+         class="text-text underline underline-offset-[3px] decoration-1 hover:text-red transition-colors"
+         >テキサスA&M大学</a
+       >機械工学科3年生。数学副専攻。現在、テキサス州最大の学生主導ロボット工学団体<a
+         href="https://turtlerobotics.org"
+         class="text-text underline underline-offset-[3px] decoration-1 hover:text-yellow transition-colors"
+         >「TURTLE Robotics」</a
+       >の内部担当副代表を務めています。また、<a
+         href="https://herc.engr.tamu.edu/"
+         class="text-text underline underline-offset-[3px] decoration-1 hover:text-peach transition-colors"
+         >「Human-Empowering Robotics and Control（HERC）Lab」</a
+       >での研究活動にも協力しています。`,
+  sections: [
+    {
+      title: "プロジェクト",
+      description: "制作物",
+      href: "/ja/projects",
+    },
+    {
+      title: "興味",
+      description: "好きなもの",
+      href: "/ja/interests",
+    },
+    {
+      title: "自己紹介",
+      description: "私は誰か？",
+      href: "/ja/aboutme",
+    },
+  ],
+};
